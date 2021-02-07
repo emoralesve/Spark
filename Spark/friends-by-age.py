eb50d2307b8200845d053304ba95d06170f86480
emoralesve <emorales@ing.ucsc.cl>
2021-02-07 18:37:18 -0300

    actualizacion

---
+++ Spark/friends-by-age.py
@@ -0,0 +1,26 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("FriendsByAge")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    age = int(fields[2])
+    numFriends = int(fields[3])
+    return (age, numFriends)
+
+
+#Spark can do special stuff with KEY/VALUE DATA
+#reduceByKey():combine values with the same key using some funcion. rdd.reduceByKey(lambda x,y:x+y) adds themm up
+#groupByKey():Group values with the same key
+#sortByKey() Sort RDD by key Values
+#keys(), values()- Create an RDD of just he keys, or just the values
+#Join, righOuterJoin, leftOuterJoin, cogroup, subtractByKey
+#We'll look at an exa
+lines = sc.textFile("C:/Users/eduardo/Desktop/SparkCourse/Spark/fakefriends.csv")
+rdd = lines.map(parseLine)
+totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
+results = averagesByAge.collect()
+for result in results:
+    print(result)
